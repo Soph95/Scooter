d62950b1bf7ef2dab80b27897016566f1d46a0ec
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2021-03-29 17:45:38 +0100

    Create return method

--- src/user.js
+++ src/user.js
@@ -1,6 +1,20 @@
+const { Scooter, ElectricScooter } = require("../src/scooter");
+const HiringStn = require("../src/stations");
+
 class User {
   constructor(name) {
     this.name = name;
+    this.scooter = null;
+  }
+  returnScooter(hiringStation) {
+    if (!this.scooter) return null;
+    if (this.scooter instanceof Scooter) {
+      hiringStation.manualScooters.push(this.scooter);
+      this.scooter = null;
+    } else if (this.scooter instanceof ElectricScooter) {
+      hiringStation.ElectricScooters.push(this.scooter);
+      this.scooter = null;
+    }
   }
 }
 module.exports = User;
