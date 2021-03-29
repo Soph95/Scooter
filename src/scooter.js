e012b18e415d52bf03b9a38b15b3b74716ee022b
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2021-03-29 17:42:20 +0100

    Minor change

--- src/scooter.js
+++ src/scooter.js
@@ -1,15 +1,20 @@
 class Scooter {
   constructor(model) {
-    console.log("scooter");
     this.model = model;
   }
 }
-//test does not pass if extending
+
 class ElectricScooter {
   constructor(model) {
-    console.log("electric scooter");
     this.model = model;
     this.charged = false;
   }
 }
+//test does not pass if extending - overides electric scooter.
+// class ElectricScooter extends Scooter {
+//   constructor(model) {
+//     super(model);
+//     this.charged = false;
+//   }
+// }
 module.exports = { Scooter, ElectricScooter };
