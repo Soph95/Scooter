a01bed83f47a392caf6c16b30d1a8dcca3360b9c
Sophia Mcgeown <sopmcgeo@WKMZT3A84AC1.global.publicisgroupe.net>
2021-03-29 17:44:34 +0100

    Create add scooter method and change hire method

--- src/stations.js
+++ src/stations.js
@@ -3,26 +3,33 @@ const { Scooter, ElectricScooter } = require("../src/scooter");
 class HiringStn {
   constructor(location) {
     this.location = location;
-    this.electricScooter = [];
-    this.manualScooter = [];
+    this.electricScooters = [];
+    this.manualScooters = [];
+  }
+  addScooter(scooter) {
+    if (scooter instanceof Scooter) {
+      this.manualScooters.push(scooter);
+    } else if (scooter instanceof ElectricScooter) {
+      this.electricScooters.push(scooter);
+    }
   }
 
   hireScooter(scooter, user) {
     if (scooter instanceof Scooter) {
-      this.manualScooter.push(user);
-      this.manualScooter.push(scooter);
+      user.scooter = scooter;
+      const index = this.manualScooters.indexOf(scooter);
+      this.manualScooters.splice(index, 1);
     } else if (scooter instanceof ElectricScooter) {
-      this.electricScooter.push(user);
-      this.electricScooter.push(scooter);
+      user.scooter = scooter;
+      const index = this.electricScooters.indexOf(scooter);
+      this.electricScooters.splice(index, 1);
     }
   }
 
   async charge(electricScooter) {
-    console.log("Starting charge");
-
     await new Promise((resolve) => setTimeout(resolve, 2000)); // wait 2 seconds
     electricScooter.charged = true;
-    console.log("Charge complete"); // after 2 seconds this line will print
+    // after 2 seconds this line will print
   }
 }
 
